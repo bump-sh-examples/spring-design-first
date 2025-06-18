5b30456a97d33e27dd05cf5e206763240698038e
Bence Erős <ebence88@gmail.com>
2025-06-18 06:03:08 +0200

    integrate kappa-spring and delete OpenApiRequestValidationFilter

--- src/main/java/com/bumpsh/demo/DemoApplication.java
+++ src/main/java/com/bumpsh/demo/DemoApplication.java
@@ -1,13 +1,28 @@
 package com.bumpsh.demo;
 
+import com.github.erosb.kappa.autoconfigure.EnableKappaRequestValidation;
+import com.github.erosb.kappa.autoconfigure.KappaSpringConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.LinkedHashMap;
 
 @SpringBootApplication
+@EnableKappaRequestValidation
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	public KappaSpringConfiguration kappaSpringConfiguration() {
+		KappaSpringConfiguration kappaConfig = new KappaSpringConfiguration();
+		var pathPatternToOpenapiDescription = new LinkedHashMap<String, String>();
+		pathPatternToOpenapiDescription.put("/**", "/api/openapi.yaml");
+		kappaConfig.setOpenapiDescriptions(pathPatternToOpenapiDescription);
+		return kappaConfig;
 	}
 
 }

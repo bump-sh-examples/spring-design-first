9819593dd3ca9c90ab907c7bad7cbd8e7a0d92de
Phil Sturgeon <67381+philsturgeon@users.noreply.github.com>
2025-06-18 12:57:09 +0100

    added proper openapi

--- src/main/java/com/bumpsh/demo/DemoApplication.java
+++ src/main/java/com/bumpsh/demo/DemoApplication.java
@@ -2,8 +2,8 @@ package com.bumpsh.demo;
 
 import com.github.erosb.kappa.autoconfigure.EnableKappaRequestValidation;
 import com.github.erosb.kappa.autoconfigure.KappaSpringConfiguration;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.Bean;
 
 import java.util.LinkedHashMap;
